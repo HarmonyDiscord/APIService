9c547685a8fead5f9c3aa09902e5bce573a7784f
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2026-01-21 09:54:43 +0000

    update feed

--- src/routes/content/feed/route.ts
+++ src/routes/content/feed/route.ts
@@ -2,6 +2,8 @@ import Elysia from 'elysia';
 import parseSearchResult from '../../../util/parseSearchResult';
 import { getYTMusic } from '../../../util/ytmusic';
 
+export const EXCLUDED_ARTISTS = process.env['EXCLUDED_ARTISTS']?.split(', ');
+
 export const contentFeedRoute = new Elysia({
 	name: 'routes:contentFeedRoute',
 	prefix: '/api/content/feed'
@@ -13,7 +15,9 @@ export const contentFeedRoute = new Elysia({
 		try {
 			const results = await ytmusic.getPlaylistVideos('PLOHoVaTp8R7d3L_pjuwIa6nRh4tH5nI4x');
 
-			return results.map((result) => parseSearchResult(result));
+			return results
+				.map((result) => parseSearchResult(result))
+				.filter((x) => 'artist' in x && x.artist?.id && EXCLUDED_ARTISTS?.includes(x.artist.id));
 		} catch {
 			return [];
 		}
